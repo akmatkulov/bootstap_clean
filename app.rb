ed000850c863a67da3044809aa5055692e2de9d6
sovetbek <akmatkulovs991@gmail.com>
2022-01-01 01:32:44 +0300

    Update visit method

--- app.rb
+++ app.rb
@@ -20,5 +20,16 @@ get '/visit' do
 end
 
 post '/visit' do
-	erb :visit
+	@user_name = params[:user_name]
+	@phone = params[:phone_number]
+	@date_time = params[:date_time]
+
+	@title = "Thank you!"
+	@message = "Dear #{@user_name}, we'll be waiting for you at #{@date_time}."
+
+	f = File.open './public/user.txt', 'a'
+	f.write "User: #{@user_name}, Phone: #{@phone}, Date and Time: #{@date_time}."
+	f.close
+
+		erb :message
 end
