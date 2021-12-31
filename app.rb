36098f868216875e4ff1d720e0e7d42f14d91e38
sovetbek <akmatkulovs991@gmail.com>
2022-01-01 02:20:32 +0300

    Add contacts.erb add cotacts.txt to gitignore

--- app.rb
+++ app.rb
@@ -30,6 +30,24 @@ post '/visit' do
 	f = File.open './public/user.txt', 'a'
 	f.write "User: #{@user_name}, Phone: #{@phone}, Date and Time: #{@date_time}."
 	f.close
+	erb :message
+end
 
-		erb :message
+# get Contacts
+get '/contacts' do
+	erb :contacts
+end
+
+post '/contacts' do
+	@user_email = params[:user_email]
+	@user_text = params[:user_text]
+
+	@title = "Thank you!"
+	@message = "Dear #{@user_email} Your message has been sent."
+
+	f = File.open './public/contacts.txt', 'a'
+	f.write "User: #{@user_email}, Text: #{@user_text}."
+	f.close
+
+	erb :message
 end
