4d4cb284648cd8a42d9ac26cfe3b7fe1481734bd
sovetbek <akmatkulovs991@gmail.com>
2022-01-01 00:57:51 +0300

    Add visit page add update app.rb

--- app.rb
+++ app.rb
@@ -3,10 +3,22 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+# main page
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
 
+
+# about
 get '/about' do
 	erb :about
+end
+
+# visit
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	erb :visit
 end
